4695c9b56d1ae4f3332069b226fb44c57af819fc
mik <autoclik974@gmail.com>
2018-11-26 17:39:34 +0100

    ajout sign up login dans navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_11_26_152237) do
-
+ActiveRecord::Schema.define(version: 2018_11_26_152722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -25,9 +22,8 @@ ActiveRecord::Schema.define(version: 2018_11_26_152237) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,6 +34,6 @@ ActiveRecord::Schema.define(version: 2018_11_26_152237) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
+end

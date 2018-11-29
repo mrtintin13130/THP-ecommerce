0386c1eea071b3e8471a236132342050a404b530
Martin <mts13@hotmail.fr>
2018-11-29 12:18:30 +0100

    user profile + edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :products
   devise_for :users
+  get '/profile', to: 'profile#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
